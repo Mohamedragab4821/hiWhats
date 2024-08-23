ecc1a6dea65351676b2c9f45c65769ef643d82cc
Mahmoud174200 <afmahmoudahmed@gmail.com>
2024-08-23 17:45:21 +0300

    home

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('home');
-});
 
-Route::post('/', [AuthController::class, 'home'])->name('home');
+
+Route::get('/', [AuthController::class, 'home'])->name('home');
 Route::post('/signin', [AuthController::class, 'login'])->name('signin');
 Route::post('/Registeration', [AuthController::class, 'Registeration'])->name('Registeration');
