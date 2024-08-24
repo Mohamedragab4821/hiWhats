f617c03fb55166c905fcbf8ba4e7706714e5bbf4
Mohamed Ragab <medoragab482@gmail.com>
2024-08-24 20:00:34 +0300

    changes

--- resources/views/favorites.blade.php
+++ resources/views/favorites.blade.php
@@ -115,109 +115,14 @@
     <main class="page-wrapper">
       <!-- Navbar for NFT Marketplace demo-->
       <!-- Remove "navbar-sticky" class to make navigation bar scrollable with the page.-->
-      <header class="navbar d-block navbar-sticky navbar-expand-lg navbar-light bg-light">
-        <div class="container"><a class="navbar-brand d-none d-sm-block me-4 order-lg-1" href="index.html"><img src="img/logo-dark.png" width="142" alt="Cartzilla"></a><a class="navbar-brand d-sm-none me-2 order-lg-1" href="index.html"><img src="img/logo-icon.png" width="74" alt="Cartzilla"></a>
-          <div class="navbar-toolbar d-flex align-items-center order-lg-3">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"><span class="navbar-toggler-icon"></span></button><a class="navbar-tool d-none d-lg-flex" href="javascript:void(0)" data-bs-toggle="collapse" data-bs-target="#searchBox" role="button" aria-expanded="false" aria-controls="searchBox"><span class="navbar-tool-tooltip">Search</span>
-              <div class="navbar-tool-icon-box"><i class="navbar-tool-icon ci-search"></i></div></a>
-            <div class="navbar-tool dropdown ms-lg-3 ms-2"><a class="navbar-tool-icon-box d-inline-block w-auto h-auto dropdown-toggle mt-n1" href="nft-account-settings.html"><img src="img/nft/vendor/avatar-sm.png" width="36" alt="user"></a>
-              <div class="dropdown-menu dropdown-menu-end">
-                <div style="min-width: 14rem;">
-                  <h6 class="dropdown-header">@foxnet_creator</h6><a class="dropdown-item d-flex align-items-center" href="nft-account-settings.html"><i class="ci-settings opacity-60 me-2"></i>Profile Settings</a><a class="dropdown-item d-flex align-items-center" href="nft-account-my-items.html"><i class="ci-image opacity-60 me-2"></i>My Items<span class="fs-xs text-muted ms-auto">5</span></a><a class="dropdown-item d-flex align-items-center" href="nft-account-my-collections.html"><i class="ci-view-list opacity-60 me-2"></i>My Collections<span class="fs-xs text-muted ms-auto">2</span></a><a class="dropdown-item d-flex align-items-center" href="nft-account-favorites.html"><i class="ci-heart opacity-60 me-2"></i>Favorites<span class="fs-xs text-muted ms-auto">3</span></a><a class="dropdown-item d-flex align-items-center" href="nft-account-notifications.html"><i class="ci-bell opacity-60 me-2"></i>Notifications</a>
-                </div>
-              </div>
-            </div><a class="btn btn-sm btn-accent rounded-1 ms-lg-4 ms-3" href="nft-create-item.html">Create</a>
-          </div>
-          <div class="collapse navbar-collapse me-auto order-lg-2" id="navbarCollapse">
-            <!-- Search (mobile)-->
-            <div class="d-lg-none py-3">
-              <div class="input-group"><i class="ci-search position-absolute top-50 start-0 translate-middle-y ms-3"></i>
-                <input class="form-control rounded-start" type="text" placeholder="What do you need?">
-              </div>
-            </div>
-            <!-- Primary menu-->
-            <ul class="navbar-nav">
-              <li class="nav-item dropdown"><a class="nav-link" href="home-nft.html">Home</a></li>
-              <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Catalog</a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="nft-catalog-v1.html">Catalog v.1</a></li>
-                  <li><a class="dropdown-item" href="nft-catalog-v2.html">Catalog v.2</a></li>
-                  <li><a class="dropdown-item" href="nft-single-auction-live.html">Single Item - Auction Live</a></li>
-                  <li><a class="dropdown-item" href="nft-single-auction-ended.html">Single Item - Auction Ended</a></li>
-                  <li><a class="dropdown-item" href="nft-single-buy.html">Single Item - Buy Now</a></li>
-                  <li><a class="dropdown-item" href="nft-vendor.html">Vendor Page</a></li>
-                  <li><a class="dropdown-item" href="nft-connect-wallet.html">Connect Wallet</a></li>
-                  <li><a class="dropdown-item" href="nft-create-item.html">Create New Item</a></li>
-                </ul>
-              </li>
-              <li class="nav-item dropdown active"><a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Account</a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="nft-account-settings.html">Profile Settings</a></li>
-                  <li><a class="dropdown-item" href="nft-account-my-items.html">My Items</a></li>
-                  <li><a class="dropdown-item" href="nft-account-my-collections.html">My Collections</a></li>
-                  <li><a class="dropdown-item" href="nft-account-favorites.html">Favorites</a></li>
-                  <li><a class="dropdown-item" href="nft-account-notifications.html">Notifications</a></li>
-                  <li><a class="dropdown-item" href="nft-account-notifications.html">User Management</a></li>
-                </ul>
-              </li>
-              <li class="nav-item"><a class="nav-link" href="index.html">Back to main demo</a></li>
-            </ul>
-          </div>
-        </div>
-        <!-- Search collapse-->
-        <div class="search-box collapse" id="searchBox">
-          <div class="container py-2">
-            <div class="input-group"><i class="ci-search position-absolute top-50 start-0 translate-middle-y ms-3"></i>
-              <input class="form-control rounded-start" type="text" placeholder="What do you need?">
-            </div>
-          </div>
-        </div>
-      </header>
-      <div class="page-title-overlap bg-accent pt-4">
-        <div class="container d-flex flex-wrap flex-sm-nowrap justify-content-center justify-content-sm-between align-items-center mb-2 pt-2">
-          <div class="d-flex align-items-center">
-            <div class="img-thumbnail rounded-circle position-relative flex-shrink-0" style="width: 6.375rem;"><img class="rounded-circle" src="img/nft/vendor/avatar.png" alt="@foxnet_creator"></div>
-            <div class="ps-3">
-              <h3 class="h5 mb-2 text-light">@foxnet_creator</h3><span class="d-block text-light fs-sm opacity-60">Joined July 2021</span>
-            </div>
-          </div>
-          <div class="my-sm-0 my-3 text-sm-end pt-1">
-            <div class="d-flex align-items-center text-nowrap fs-sm">
-              <div class="mb-2 me-sm-3 me-2 text-muted"><span class='fw-medium text-light'>766</span> <span class='text-white opacity-70'>followers</span></div>
-              <div class="mb-2 ps-sm-3 ps-2 border-start border-light text-muted"><span class='fw-medium text-light'>2K</span> <span class='text-white opacity-70'>following</span></div>
-            </div><a class="text-light" href="#">0x1dDB2C08s97...9Ecd<i class="ci-copy ms-2 fs-sm" data-bs-toggle="tooltip" data-bs-placement="top" title="Copy"></i></a>
-          </div>
-        </div>
-      </div>
+      @include('Includes.account_header')
+
+      @include('Includes.navBar')
       <div class="container mb-5 pb-3">
         <div class="bg-light shadow-lg rounded-3 overflow-hidden">
           <div class="row">
             <!-- Sidebar-->
-            <aside class="col-lg-3 pe-xl-5">
-              <!-- Account menu toggler (hidden on screens larger 992px)-->
-              <div class="d-block d-lg-none p-4"><a class="btn btn-outline-accent d-block" href="#account-menu" data-bs-toggle="collapse"><i class="ci-menu me-2"></i>Account menu</a></div>
-              <!-- Actual menu-->
-              <div class="h-100 border-end mb-2">
-                <div class="d-lg-block collapse" id="account-menu">
-                  <ul class="list-unstyled mb-0">
-                    <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="nft-account-settings.html"><i class="ci-settings opacity-60 me-2"></i>Profile Settings</a></li>
-                    <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="nft-account-my-items.html"><i class="ci-image opacity-60 me-2"></i>My Items<span class="fs-sm text-muted ms-auto">5</span></a></li>
-                    <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="nft-account-my-collections.html"><i class="ci-view-list opacity-60 me-2"></i>My Collections<span class="fs-sm text-muted ms-auto">2</span></a></li>
-                    <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="nft-account-favorites.html"><i class="ci-heart opacity-60 me-2"></i>Favorites<span class="fs-sm text-muted ms-auto">3</span></a></li>
-                    <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="nft-account-notifications.html"><i class="ci-bell opacity-60 me-2"></i>Notifications</a></li>
-                    <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="{{ route('productMangement') }}"><i class="ci-bell opacity-60 me-2"></i>Product Mangement</a></li>
-                    <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="{{ route('categoryMangement') }}"><i class="ci-bell opacity-60 me-2"></i>Category Mangement</a></li>
-                    <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="nft-account-notifications.html"><i class="ci-bell opacity-60 me-2"></i>Notifications</a></li>
-                    <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="{{route('userManagement')}}"><i class="ci-bell opacity-60 me-2"></i>User Management</a></li>
-                    <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="nft-account-notifications.html"><i class="ci-bell opacity-60 me-2"></i>Messages</a></li>
-                    <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="{{route('settings.index')}}"><i class="ci-sign-out opacity-60 me-2"></i>Settings</a></li>
-                    <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="{{route('favorites.index')}}"><i class="ci-sign-out opacity-60 me-2"></i>Favorites</a></li>
-
-                    <li class="border-bottom mb-0"><a class="nav-link-style d-flex align-items-center px-4 py-3" href="account-signin.html"><i class="ci-sign-out opacity-60 me-2"></i>Sign out</a></li>
-                  </ul>
-                </div>
-              </div>
-            </aside>
+            @include('Includes.leftSideMenue')
             <!-- Content-->
             <section class="col-lg-9 pt-lg-4 pb-4 mb-3">
                 <div class="container py-lg-4">
@@ -276,75 +181,8 @@
       </div>
     </main>
     <!-- Footer-->
-    <footer class="footer bg-darker">
-      <div class="bg-dark">
-        <div class="container py-5">
-          <div class="row py-lg-4">
-            <div class="col-lg-4 mb-lg-0 mb-4">
-              <div class="widget pb-3 mb-lg-4">
-                <h3 class="widget-title text-light pb-1">Stay informed</h3>
-                <form class="subscription-form validate" action="https://studio.us12.list-manage.com/subscribe/post?u=c7103e2c981361a6639545bd5&amp;amp;id=29ca296126" method="post" name="mc-embedded-subscribe-form" target="_blank" novalidate>
-                  <div class="input-group flex-nowrap"><i class="ci-mail position-absolute top-50 translate-middle-y text-muted fs-base ms-3"></i>
-                    <input class="form-control rounded-start" type="email" name="EMAIL" placeholder="Your email" required>
-                    <button class="btn btn-accent" type="submit" name="subscribe">Subscribe*</button>
-                  </div>
-                  <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                  <div style="position: absolute; left: -5000px;" aria-hidden="true">
-                    <input class="subscription-form-antispam" type="text" name="b_c7103e2c981361a6639545bd5_29ca296126" tabindex="-1">
-                  </div>
-                  <div class="form-text text-light opacity-50">*Subscribe to our newsletter to receive early discount offers, updates and new products info.</div>
-                  <div class="subscription-status"></div>
-                </form>
-              </div>
-              <div><a class="btn-social bs-light bs-twitter me-2 mb-2" href="#"><i class="ci-twitter"></i></a><a class="btn-social bs-light bs-facebook me-2 mb-2" href="#"><i class="ci-facebook"></i></a><a class="btn-social bs-light bs-instagram me-2 mb-2" href="#"><i class="ci-instagram"></i></a><a class="btn-social bs-light bs-pinterest me-2 mb-2" href="#"><i class="ci-pinterest"></i></a><a class="btn-social bs-light bs-youtube me-2 mb-2" href="#"><i class="ci-youtube"></i></a></div>
-            </div>
-            <div class="col-lg-6 offset-lg-1">
-              <div class="d-flex flex-sm-row flex-column justify-content-sm-between mt-n4 mx-lg-n3">
-                <div class="widget widget-links widget-light mt-4 px-lg-3 px-sm-n2">
-                  <h3 class="widget-title text-light">Company</h3>
-                  <ul class="widget-list">
-                    <li class="widget-list-item"><a class="widget-list-link" href="#">About Us</a></li>
-                    <li class="widget-list-item"><a class="widget-list-link" href="#">How It Works</a></li>
-                    <li class="widget-list-item"><a class="widget-list-link" href="#">Create an NFT With Us</a></li>
-                    <li class="widget-list-item"><a class="widget-list-link" href="#">Support</a></li>
-                    <li class="widget-list-item"><a class="widget-list-link" href="#">Blog</a></li>
-                    <li class="widget-list-item"><a class="widget-list-link" href="#">My Account</a></li>
-                  </ul>
-                </div>
-                <div class="widget widget-links widget-light mt-4 px-lg-3 px-sm-n2">
-                  <h3 class="widget-title text-light">Marketplace</h3>
-                  <ul class="widget-list">
-                    <li class="widget-list-item"><a class="widget-list-link" href="#">All NFTs</a></li>
-                    <li class="widget-list-item"><a class="widget-list-link" href="#">New</a></li>
-                    <li class="widget-list-item"><a class="widget-list-link" href="#">Art</a></li>
-                    <li class="widget-list-item"><a class="widget-list-link" href="#">Music</a></li>
-                    <li class="widget-list-item"><a class="widget-list-link" href="#">Photography</a></li>
-                    <li class="widget-list-item"><a class="widget-list-link" href="#">Utility</a></li>
-                  </ul>
-                </div>
-                <div class="widget widget-links widget-light mt-4 px-lg-3 px-sm-n2">
-                  <h3 class="widget-title text-light">Help</h3>
-                  <ul class="widget-list">
-                    <li class="widget-list-item"><a class="widget-list-link" href="#">Terms and Conditions</a></li>
-                    <li class="widget-list-item"><a class="widget-list-link" href="#">Privacy Policy</a></li>
-                    <li class="widget-list-item"><a class="widget-list-link" href="#">FAQ</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="container my-lg-4 my-3 py-2">
-        <div class="d-flex flex-md-row flex-column-reverse align-items-center justify-content-md-between">
-          <div class="fs-xs text-light opacity-50">&copy; All rights reserved. Made by <a class="text-light" href="https://createx.studio/" target="_blank" rel="noopener">Createx Studio</a></div>
-          <div class="d-flex align-items-start mb-md-0 mb-3 mx-n1">
-            <div class="px-1"><a class="btn-market btn-apple bg-dark" href="#"><span class="btn-market-subtitle">Download on the</span><span class="btn-market-title">App Store</span></a></div>
-            <div class="px-1"><a class="btn-market btn-google bg-dark" href="#"><span class="btn-market-subtitle">Download on the</span><span class="btn-market-title">Google Play</span></a></div>
-          </div>
-        </div>
-      </div>
-    </footer>
+    @include('Includes.footer')
+
     <!-- Toolbar for handheld devices (NFT Marketplace)-->
     <div class="handheld-toolbar">
       <div class="d-table table-layout-fixed w-100"><a class="d-table-cell handheld-toolbar-item" href="nft-account-settings.html" data-bs-toggle="modal"><span class="handheld-toolbar-icon"><i class="ci-user"></i></span><span class="handheld-toolbar-label">Account</span></a><a class="d-table-cell handheld-toolbar-item" href="javascript:void(0)" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" onclick="window.scrollTo(0, 0)"><span class="handheld-toolbar-icon"><i class="ci-menu"></i></span><span class="handheld-toolbar-label">Menu</span></a><a class="d-table-cell handheld-toolbar-item" href="nft-create-item.html"><span class="handheld-toolbar-icon"><i class="ci-add"></i></span><span class="handheld-toolbar-label">Create item</span></a>
